93ae7333dbc2ca471adcd6f8af0e0a4232a906cc
pas2704 <pas2704@outlook.com>
2024-04-24 13:32:06 -0400

    Change to check whether the textbox is focused instead of the name being changed.

--- GridRenameFix/MyTerminalInfoControllerPatch.cs
+++ GridRenameFix/MyTerminalInfoControllerPatch.cs
@@ -1,6 +1,4 @@
 ﻿using HarmonyLib;
-using Sandbox.Game.Entities;
-using Sandbox.Game.World;
 using Sandbox.Graphics.GUI;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +10,6 @@ namespace GridRenameFix
     [HarmonyPatch]
     internal static class MyTerminalInfoControllerPatch
     {
-        private static MyCubeGrid _lastGrid;
-
-
-        static MyTerminalInfoControllerPatch()
-        {
-            MySession.OnUnloading += () => _lastGrid = null;
-        }
-
         [HarmonyPatch("Sandbox.Game.Gui.MyTerminalInfoController", "UpdateBeforeDraw")]
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
@@ -32,23 +22,11 @@ namespace GridRenameFix
             list[index + 4].labels.Add(label);
 
             list.Insert(index, new CodeInstruction(OpCodes.Brtrue, label));
-            list.Insert(index, CodeInstruction.Call(typeof(MyTerminalInfoControllerPatch), nameof(ShouldIgnoreChange)));
-            list.Insert(index, new CodeInstruction(OpCodes.Callvirt, 
-                AccessTools.PropertyGetter(typeof(MyGuiControlTextbox), nameof(MyGuiControlTextbox.Text))));
+            list.Insert(index, CodeInstruction.Call(typeof(MyGuiControlTextbox), "get_HasFocus"));
             list.Insert(index, new CodeInstruction(OpCodes.Ldloc_1));
-            list.Insert(index, new CodeInstruction(OpCodes.Ldsfld, AccessTools.Field("Sandbox.Game.Gui.MyTerminalInfoController:m_grid")));
 
             return list;
         }
 
-        private static bool ShouldIgnoreChange(MyCubeGrid grid, string text)
-        {
-            var ignore = _lastGrid != null && grid == _lastGrid && grid.DisplayName != text;
-            _lastGrid = grid;
-            return ignore;
-        }
-        [HarmonyPatch("Sandbox.Game.Gui.MyTerminalInfoController", "MarkControlsDirty")]
-        [HarmonyPostfix]
-        private static void Postfix() => _lastGrid = null;
     }
 }
